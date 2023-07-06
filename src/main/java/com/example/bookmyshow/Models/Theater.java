a0f8dca89ea03ba39883da48681657d346e71cae
Ajaykumaryd <ajaykumaryd2001@gmail.com>
2023-07-06 20:46:10 +0530

    added

---
+++ src/main/java/com/example/bookmyshow/Models/Theater.java
@@ -0,0 +1,40 @@
+package com.example.bookmyshow.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name = "theater")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Theater {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String location;
+
+
+    @OneToMany(mappedBy = "theater",cascade = CascadeType.ALL)
+    private List<TheaterSeat> theaterSeatList = new ArrayList<>();
+
+
+
+
+
+
+
+}
